402b9215a47bb4aff27692c91ac6845936d553cc
Safidy Sylvana Nambinjanahary <sitehars@gmail.com>
2026-08-18 13:17:27 +0300

    Remove Lovable references

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,7 +10,6 @@ import {
 import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
-import { reportLovableError } from "../lib/lovable-error-reporting";
 
 function NotFoundComponent() {
   return (
@@ -37,7 +36,6 @@ function NotFoundComponent() {
 function ErrorComponent({ error, reset }: { error: Error; reset: () => void }) {
   const router = useRouter();
   useEffect(() => {
-    reportLovableError(error, { boundary: "tanstack_root_error_component" });
   }, [error]);
 
   return (
